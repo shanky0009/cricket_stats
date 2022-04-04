a293568b31e6a7d44eaca36a1c42ade21bdc145d
shanky0009 <shashank190897@gmail.com>
2022-04-04 10:44:55 +0530

    updated dag

--- data_ingestion/dags/cric_data_ingestion_dag.py
+++ data_ingestion/dags/cric_data_ingestion_dag.py
@@ -1,3 +1,4 @@
+from cProfile import run
 import os
 import logging
 from pathlib import Path
@@ -16,27 +17,44 @@ import glob
 import json as js
 import pandas as pd
 import datetime
-
+import pickle
+import numpy as np
 
 PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
 BUCKET = os.environ.get("GCP_GCS_BUCKET")
 AIRFLOW_HOME = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
 
 year = '{{ execution_date.strftime(\'%Y\') }}'
-dataset_file_template = AIRFLOW_HOME + "/{{ execution_date.strftime(\'%Y\') }}_json.zip"
+DATASET_FILE_TEMPLATE = AIRFLOW_HOME + "/{{ execution_date.strftime(\'%Y\') }}_json.zip"
 dataset_url = "https://cricsheet.org/downloads/{{ execution_date.strftime(\'%Y\') }}_json.zip"
-match_info_csv_file_template = AIRFLOW_HOME + "/match_info/match_info_{{ execution_date.strftime(\'%Y\') }}.csv"
-match_info_parquet_file_template = AIRFLOW_HOME + "/match_info/match_info_{{ execution_date.strftime(\'%Y\') }}.parquet"
-innings_info_file_template = AIRFLOW_HOME + "/innings/{{ execution_date.strftime(\'%Y\') }}"
-dataset_output_folder_template = AIRFLOW_HOME + "/{{ execution_date.strftime(\'%Y\') }}"
-innings_gcloud_upload_folder = "innings/{{ execution_date.strftime(\'%Y\') }}"
+MATCH_INFO_PARQUET_FILE_TEMPLATE = AIRFLOW_HOME + "/match_info/match_info_{{ execution_date.strftime(\'%Y\') }}.parquet"
+MATCH_INFO_GCS_FILE_TEMPLATE = "raw/match_info/match_info_{{ execution_date.strftime(\'%Y\') }}.parquet"
+INNINGS_RUNS_PICKLE_FILE_TEMPLATE = AIRFLOW_HOME + "/innings/runs_{{ execution_date.strftime(\'%Y\') }}.p"
+INNINGS_RUNS_PARQUET_FILE_TEMPLATE = AIRFLOW_HOME + "/innings/runs_{{ execution_date.strftime(\'%Y\') }}.parquet"
+INNINGS_RUNS_GCS_FILE_TEMPLATE = "raw/innings/runs_{{ execution_date.strftime(\'%Y\') }}.parquet"
+INNINGS_WICKETS_PICKLE_FILE_TEMPLATE = AIRFLOW_HOME + "/innings/wickets_{{ execution_date.strftime(\'%Y\') }}.p"
+INNINGS_WICKETS_PARQUET_FILE_TEMPLATE = AIRFLOW_HOME + "/innings/wickets_{{ execution_date.strftime(\'%Y\') }}.parquet"
+INNINGS_WICKETS_GCS_PARQUET_FILE_TEMPLATE = "raw/innings/wickets_{{ execution_date.strftime(\'%Y\') }}.parquet"
+DATASET_OUTPUT_FOLDER_TEMPLATE = AIRFLOW_HOME + "/{{ execution_date.strftime(\'%Y\') }}"
+INNINGS_GCLOUD_UPLOAD_FOLDER = "innings/{{ execution_date.strftime(\'%Y\') }}"
 
 
 path_to_local_home = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
-parquet_file = dataset_file_template.replace('.csv', '.parquet')
+parquet_file = DATASET_FILE_TEMPLATE.replace('.csv', '.parquet')
 BIGQUERY_DATASET = os.environ.get("BIGQUERY_DATASET", 'cricket_data')
 
-match_info_keys_required = ['city', 'dates', 'event', 'gender', 'match_type', 'match_type_number', 'officials', 'outcome', 'player_of_match', 'season', 'team_type', 'teams', 'toss', 'venue', 'match_id']
+match_info_keys_required = ['city', 'dates', 'match_type', 'officials', 'outcome', 'player_of_match', 'season', 'team_type', 'teams', 'toss', 'venue', 'id']
+
+def dump_pickle(file_name, data):
+    with open(file_name, 'wb') as f:
+       pickle.dump(data, f)
+
+
+def load_pickle(filename):
+    with open(filename, 'rb') as f:
+        data = pickle.load(f)
+    return data
+
 
 def upload_to_gcs(bucket, object_name, local_file):
     """
@@ -104,13 +122,16 @@ def process_match_info(source_folder, output_file):
     match_info_df.to_parquet(output_file)
 
 
-def process_deliveries_data(source_folder, output_folder):
+def process_deliveries_data(source_folder, runs_pickle_file, wickets_pickle_file):
+    print(runs_pickle_file, wickets_pickle_file)
     glob_search_path = os.path.join(source_folder, "*.json")
     json_paths = glob.glob(glob_search_path)
-    print(f'******_*____________________************________{len(json_paths)}_______________***********')
-    output_folder = Path(output_folder)
-    output_folder.mkdir(parents=True, exist_ok=True)
-    print(output_folder)
+    runs_output_folder = Path(runs_pickle_file).parent
+    runs_output_folder.mkdir(parents=True, exist_ok=True)
+    wickets_output_folder = Path(wickets_pickle_file).parent
+    wickets_output_folder.mkdir(parents=True, exist_ok=True)
+    innings_runs = []
+    innings_wickets = []
     for json_path in json_paths:
         with open(json_path, 'r') as f:
             data = js.load(f)
@@ -118,11 +139,55 @@ def process_deliveries_data(source_folder, output_folder):
         for i, innings in enumerate(data['innings']):
             if 'overs' not in innings.keys():
                 continue
-            overs = innings['overs']
-            df = pd.DataFrame(overs)
-            df['match_id'] = match_id
-            df['innings'] = i+1
-            df.to_parquet(output_folder / f'{match_id}_{i+1}.parquet')
+            for over in innings['overs']:
+                over_no = over['over']
+                for ball_no, ov in enumerate(over['deliveries'], 1):
+                    ov['over_no'] = over_no
+                    ov['ball_no'] = ball_no
+                    ov['match_id'] = match_id
+                    ov['innings'] = i
+                    if 'wickets' in ov:
+                        # add wickets data to innings_wickets
+                        if len(ov['wickets']) > 1:
+                            print("F")
+                            print(ov)
+                            break
+                        ov['wickets'] = ov['wickets'][0]
+                        innings_wickets.append(ov)
+                    else:
+                        innings_runs.append(ov)
+    dump_pickle(runs_pickle_file, innings_runs)
+    dump_pickle(wickets_pickle_file, innings_wickets)
+
+
+def process_wickets_data(wickets_pickle_file, output_file):
+    innings_wickets = load_pickle(wickets_pickle_file)
+    wickets_df = pd.DataFrame(innings_wickets)
+    wickets_df['wickets'] = wickets_df['wickets'].apply(js.dumps)
+    wickets_df['runs'] = wickets_df['runs'].apply(js.dumps)
+    wickets_df['extras'] = wickets_df['extras'].apply(js.dumps)
+    wickets_df.extras.replace('NaN', np.nan, inplace=True)
+    if 'review' in wickets_df.columns:
+        wickets_df.drop(['review'], axis=1, inplace=True)
+    if 'replacements' in wickets_df.columns:
+        wickets_df.drop(['replacements'], axis=1, inplace=True)
+    # wickets_df.to_sql(name=table_name, con=engine, if_exists='append', index=False)
+    wickets_df.to_parquet(output_file)
+
+
+def process_runs_data(runs_pickle_file, output_file):
+    innings_runs = load_pickle(runs_pickle_file)
+    runs_df = pd.DataFrame(innings_runs)
+    runs_df['runs'] = runs_df['runs'].apply(js.dumps)
+    if 'replacements' in runs_df.columns:
+        runs_df['replacements'] = runs_df['replacements'].apply(js.dumps)
+        runs_df.replacements.replace('NaN', np.nan, inplace=True)
+    runs_df['extras'] = runs_df['extras'].apply(js.dumps)
+    runs_df.extras.replace('NaN', np.nan, inplace=True)
+    if 'review' in runs_df.columns:
+        runs_df.drop(['review'], axis=1, inplace=True)
+    # runs_df.to_sql(name=table_name, con=engine, if_exists='append', index=False)
+    runs_df.to_parquet(output_file)
 
 
 default_args = {
@@ -143,15 +208,15 @@ with DAG(
 
     download_dataset_task = BashOperator(
         task_id="download_dataset_task",
-        bash_command=f"curl -sSLf {dataset_url} > {dataset_file_template}"
+        bash_command=f"curl -sSLf {dataset_url} > {DATASET_FILE_TEMPLATE}"
     )
 
     unzip_file_task = PythonOperator(
         task_id="unzip_file_task",
         python_callable=unzip_file,
         op_kwargs={
-            "src_file": dataset_file_template,
-            "output_folder": dataset_output_folder_template,
+            "src_file": DATASET_FILE_TEMPLATE,
+            "output_folder": DATASET_OUTPUT_FOLDER_TEMPLATE,
         },
     )
 
@@ -159,8 +224,8 @@ with DAG(
         task_id="process_match_info_task",
         python_callable=process_match_info,
         op_kwargs={
-            "source_folder": dataset_output_folder_template,
-            "output_file": match_info_parquet_file_template
+            "source_folder": DATASET_OUTPUT_FOLDER_TEMPLATE,
+            "output_file": MATCH_INFO_PARQUET_FILE_TEMPLATE
         },
     )
 
@@ -175,7 +240,7 @@ with DAG(
         op_kwargs={
             "bucket": BUCKET,
             "object_name": f"match_info/match_info_{year}.parquet",
-            "local_file": match_info_parquet_file_template,
+            "local_file": MATCH_INFO_PARQUET_FILE_TEMPLATE,
         },
     )
 
@@ -183,19 +248,38 @@ with DAG(
         task_id="process_deliveries_data_task",
         python_callable=process_deliveries_data,
         op_kwargs={
-            "source_folder": dataset_output_folder_template,
-            "output_folder": innings_info_file_template
+            "source_folder": DATASET_OUTPUT_FOLDER_TEMPLATE,
+            "runs_pickle_file": INNINGS_RUNS_PICKLE_FILE_TEMPLATE,
+            "wickets_pickle_file": INNINGS_WICKETS_PICKLE_FILE_TEMPLATE
         },
     )
 
-    upload_deliveries_info_to_gcs_task = PythonOperator(
-        task_id="upload_deliveries_info_to_gcs_task",
-        python_callable=upload_folder_to_gcs,
+    process_wickets_data_task = PythonOperator(
+        task_id="process_wickets_data_task",
+        python_callable=process_wickets_data,
         op_kwargs={
-            "bucket": BUCKET,
-            "source_folder": innings_info_file_template,
-            "gcloud_upload_folder": innings_gcloud_upload_folder
+            "wickets_pickle_file": INNINGS_WICKETS_PICKLE_FILE_TEMPLATE,
+            "output_file": INNINGS_WICKETS_PARQUET_FILE_TEMPLATE
         },
     )
+
+    process_runs_data_task = PythonOperator(
+        task_id="process_runs_data_task",
+        python_callable=process_runs_data,
+        op_kwargs={
+            "runs_pickle_file": INNINGS_RUNS_PICKLE_FILE_TEMPLATE,
+            "output_file": INNINGS_WICKETS_PARQUET_FILE_TEMPLATE
+        },
+    )
+
+    # upload_deliveries_info_to_gcs_task = PythonOperator(
+    #     task_id="upload_deliveries_info_to_gcs_task",
+    #     python_callable=upload_folder_to_gcs,
+    #     op_kwargs={
+    #         "bucket": BUCKET,
+    #         "source_folder": innings_info_file_template,
+    #         "gcloud_upload_folder": innings_gcloud_upload_folder
+    #     },
+    # )
 
     download_dataset_task >> unzip_file_task >> process_match_info_task >> upload_match_info_to_gcs_task >> process_deliveries_data_task >> upload_deliveries_info_to_gcs_task
